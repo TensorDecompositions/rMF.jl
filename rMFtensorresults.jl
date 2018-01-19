19a0b4e2fc31325593b2bc649df7a29c0d5f1ada
monty <velimir.vesselinov@gmail.com>
2018-01-19 13:53:31 -0700

    prefix added; results added

--- rMFtensorresults.jl
+++ rMFtensorresults.jl
@@ -1,4 +1,4 @@
-function plottensorresults(X::Array, Xe::Array, W::Array, ns::Number=size(W)[2]; figuredir="tensor-figures", isn=isnan.(X), keyword="", prefix="$(rMF.case)-w$(rMF.wellssetid)-s$(rMF.speciessetid)-y$(rMF.tensoryearstep)-$keyword")
+function plottensorresults(X::Array, Xe::Array, W::Array, ns::Number=size(W)[2]; figuredir="tensor-figures", isn=isnan.(X), keyword="", prefix="$(rMF.case)-w$(rMF.wellssetid)-s$(rMF.speciessetid)-y$(rMF.tensoryearstep)-$keyword$ns-")
 	sourcenames = map(i->"S$i", 1:ns)
 
 	Z = deepcopy(X)
@@ -24,14 +24,20 @@ function plottensorresults(X::Array, Xe::Array, W::Array, ns::Number=size(W)[2];
 
 	info("Plot species concentrations for each well ...")
 	for (c, w) in enumerate(rMF.wellnameorder)
+		p = Vector{Any}(length(rMF.uniquespecies))
 		for i = 1:length(rMF.uniquespecies)
 			y1 = X[c, i, :]
 			y2 = Xe[c, i, :]
-			pl = Gadfly.layer(x=rMF.tensorperiod.+1, y=y1, Gadfly.Geom.line(), Gadfly.Theme(line_width=2Gadfly.pt, default_color=parse(Colors.Colorant, scolors[i])))
-			pd = Gadfly.layer(x=rMF.tensorperiod.+1, y=y2, Gadfly.Geom.line(), Gadfly.Theme(line_width=3Gadfly.pt, line_style=:dash,default_color=parse(Colors.Colorant, scolors[i])))
-			f = Gadfly.plot(pl..., pd..., Gadfly.Coord.Cartesian(xmin=2005, xmax=2017), Gadfly.Guide.xlabel(""), Gadfly.Guide.ylabel(rMF.uniquespecies[i]), Gadfly.Guide.title(w))
-			Gadfly.draw(Gadfly.PNG("$figuredir/$prefix-$w-$(rMF.uniquespecies[i]).png", 6Gadfly.inch, 3Gadfly.inch), f)
+			pl = Gadfly.layer(x=rMF.tensorperiod.+1, y=y1, Gadfly.Geom.line(), Gadfly.Theme(line_width=1Gadfly.pt, default_color=parse(Colors.Colorant, scolors[i])))
+			pd = Gadfly.layer(x=rMF.tensorperiod.+1, y=y2, Gadfly.Geom.line(), Gadfly.Theme(line_width=3Gadfly.pt, line_style=:dashdotdot, default_color=parse(Colors.Colorant, scolors[i])))
+			pp = Gadfly.plot(pl..., pd..., Gadfly.Coord.Cartesian(xmin=2005, xmax=2017), Gadfly.Guide.xlabel(""), Gadfly.Guide.ylabel(rMF.uniquespecies[i]), Gadfly.Guide.title(w))
+			Gadfly.draw(Gadfly.PNG("$figuredir/$prefix$w-$(rMF.uniquespecies[i]).png", 6Gadfly.inch, 3Gadfly.inch), pp)
+			p[i] = Gadfly.plot(pl..., pd..., Gadfly.Coord.Cartesian(xmin=2005, xmax=2017), Gadfly.Guide.xlabel(""), Gadfly.Guide.ylabel(rMF.uniquespecies[i]))
 		end
+		f = Compose.compose(Compose.context(0, 0, 1Compose.w, 0.001Compose.h),
+			(Compose.context(), Compose.fill("gray"), Compose.fontsize(10Compose.pt), Compose.text(0.5, 1, w, Compose.hcenter, Compose.vtop)))
+		pl = Gadfly.vstack(f, p...)
+		Gadfly.draw(Gadfly.PNG("$figuredir/$prefix$w-allconc.png", 6Gadfly.inch, 3Gadfly.inch * length(rMF.uniquespecies)), pl)
 	end
 
 	info("Plot normalized species concentrations at each well (normalized by set) ...")
@@ -46,7 +52,7 @@ function plottensorresults(X::Array, Xe::Array, W::Array, ns::Number=size(W)[2];
 			pd[i] = Gadfly.layer(x=rMF.tensorperiod.+1, y=y2, Gadfly.Geom.line(), Gadfly.Theme(line_width=2Gadfly.pt, line_style=:dash,default_color=parse(Colors.Colorant, scolors[i])))
 		end
 		f = Gadfly.plot(pl..., pd..., Gadfly.Coord.Cartesian(xmin=2005, xmax=2017, ymin=0, ymax=1), Gadfly.Guide.xlabel(""), Gadfly.Guide.ylabel("Normalized concentrations"), Gadfly.Guide.title(w), Gadfly.Guide.manual_color_key("Species", rMF.uniquespecies, scolors[1:length(rMF.uniquespecies)]))
-		Gadfly.draw(Gadfly.PNG("$figuredir/$prefix-$w-normallconc.png", 6Gadfly.inch, 3Gadfly.inch), f)
+		Gadfly.draw(Gadfly.PNG("$figuredir/$prefix$w-normallconc.png", 6Gadfly.inch, 3Gadfly.inch), f)
 		display(f)
 		println()
 	end
@@ -62,7 +68,7 @@ function plottensorresults(X::Array, Xe::Array, W::Array, ns::Number=size(W)[2];
 			pd[i] = Gadfly.layer(x=rMF.tensorperiod.+1, y=y2, Gadfly.Geom.line(), Gadfly.Theme(line_width=2Gadfly.pt, line_style=:dash,default_color=parse(Colors.Colorant, scolors[i])))
 		end
 		f = Gadfly.plot(pl..., pd..., Gadfly.Coord.Cartesian(xmin=2005, xmax=2017, ymin=0, ymax=1), Gadfly.Guide.xlabel(""), Gadfly.Guide.ylabel("Normalized concentrations"), Gadfly.Guide.title(w), Gadfly.Guide.manual_color_key("Species", rMF.uniquespecies, scolors[1:length(rMF.uniquespecies)]))
-		Gadfly.draw(Gadfly.PNG("$figuredir/$prefix-$w-normwellconc.png", 6Gadfly.inch, 3Gadfly.inch), f)
+		Gadfly.draw(Gadfly.PNG("$figuredir/$prefix$w-normwellconc.png", 6Gadfly.inch, 3Gadfly.inch), f)
 		display(f)
 		println()
 	end
@@ -74,21 +80,39 @@ function plottensorresults(X::Array, Xe::Array, W::Array, ns::Number=size(W)[2];
 			pl[i] = Gadfly.layer(x=rMF.tensorperiod.+1, y=W[c, i, :], Gadfly.Geom.line(), Gadfly.Theme(line_width=2Gadfly.pt, default_color=parse(Colors.Colorant, scolors[i])))
 		end
 		f = Gadfly.plot(pl..., Gadfly.Coord.Cartesian(xmin=2005, xmax=2017, ymin=0, ymax=1), Gadfly.Guide.xlabel(""), Gadfly.Guide.ylabel("Source mixing"), Gadfly.Guide.title(w), Gadfly.Guide.manual_color_key("Sources", sourcenames, scolors[1:ns]))
-		Gadfly.draw(Gadfly.PNG("$figuredir/$prefix-$ns-$w-mixing.png", 6Gadfly.inch, 3Gadfly.inch), f)
+		Gadfly.draw(Gadfly.PNG("$figuredir/$prefix$w-mixing.png", 6Gadfly.inch, 3Gadfly.inch), f)
 		display(f)
 		println()
 	end
+
+	# irange = [1,2,5]
+	# info("NTFk results for w$(rMF.wellssetid) s$(rMF.speciessetid) y$(rMF.tensorperiod) $ns sources; plot only sources $(irange) ...")
+	# for (c, w) in enumerate(rMF.wellnameorder)
+	# 	pl = Vector{Any}(0)
+	# 	for i = irange
+	# 		p = Gadfly.layer(x=rMF.tensorperiod.+1, y=W[c, i, :], Gadfly.Geom.line(), Gadfly.Theme(line_width=2Gadfly.pt, default_color=parse(Colors.Colorant, scolors[i])))
+	# 		push!(pl, p)
+	# 	end
+	# 	f = Gadfly.plot(pl..., Gadfly.Coord.Cartesian(xmin=2005, xmax=2017, ymin=0), Gadfly.Guide.xlabel(""), Gadfly.Guide.ylabel("Source mixing"), Gadfly.Guide.title(w), Gadfly.Guide.manual_color_key("Sources", sourcenames[irange], scolors[irange]))
+	# 	Gadfly.draw(Gadfly.PNG("$figuredir/$prefix$w-125mixing.png", 6Gadfly.inch, 3Gadfly.inch), f)
+	# 	display(f)
+	# 	println()
+	# end
 end
 
-function getalltensorresults(X::Array, nsrange; retries=1000, resultdir="tensor-results", keyword="scale-", quiet=true, isn=isnan.(X))
+function getalltensorresults(X::Array, nsrange; retries=1000, resultdir="tensor-results", keyword="scale-", prefix="", quiet=true, isn=isnan.(X))
 	Xbest = nothing
 	Wbest = nothing
 	Hbest = nothing
 	for ns = nsrange
-		if isdefined(rMF, :wellssetid)
-			filename = "$resultdir/$(rMF.case)-w$(rMF.wellssetid)-s$(rMF.speciessetid)-y$(rMF.tensoryearstep)-$keyword$ns-$retries-all.jld"
+		if prefix == ""
+			if isdefined(rMF, :wellssetid)
+				filename = "$resultdir/$(rMF.case)-w$(rMF.wellssetid)-s$(rMF.speciessetid)-y$(rMF.tensoryearstep)-$keyword$ns-$retries-all.jld"
+			else
+				filename = "$resultdir/$(rMF.case)-$keyword$ns-$retries-all.jld"
+			end
 		else
-			filename = "$resultdir/$(rMF.case)-$keyword$ns-$retries-all.jld"
+			filename = "$resultdir/$(prefix)-$keyword$ns-$retries-all.jld"
 		end
 		if isfile(filename)
 			fit, Wbest, Hbest, W, H = JLD.load(filename, "fit", "Wbest", "Hbest", "W", "H")
@@ -110,15 +134,19 @@ function getalltensorresults(X::Array, nsrange; retries=1000, resultdir="tensor-
 	return Xbest, Wbest, Hbest
 end
 
-function gettensorresults(X::Array, nsrange; retries=1000, resultdir="tensor-results", keyword="scale-", quiet=true, isn=isnan.(X))
+function gettensorresults(X::Array, nsrange; retries=1000, resultdir="tensor-results", keyword="scale-", prefix="", quiet=true, isn=isnan.(X))
 	Xe = nothing
 	W = nothing
 	H = nothing
 	for ns = nsrange
-		if isdefined(rMF, :wellssetid)
-			filename = "$resultdir/$(rMF.case)-w$(rMF.wellssetid)-s$(rMF.speciessetid)-y$(rMF.tensoryearstep)-$keyword$ns-$retries.jld"
+		if prefix == ""
+			if isdefined(rMF, :wellssetid)
+				filename = "$resultdir/$(rMF.case)-w$(rMF.wellssetid)-s$(rMF.speciessetid)-y$(rMF.tensoryearstep)-$keyword$ns-$retries.jld"
+			else
+				filename = "$resultdir/$(rMF.case)-$keyword$ns-$retries.jld"
+			end
 		else
-			filename = "$resultdir/$(rMF.case)-$keyword$ns-$retries.jld"
+			filename = "$resultdir/$(prefix)-$keyword$ns-$retries.jld"
 		end
 		if isfile(filename)
 			of, sil, aic, W, H = JLD.load(filename, "fit", "robustness", "aic", "W", "H")
@@ -133,15 +161,19 @@ function gettensorresults(X::Array, nsrange; retries=1000, resultdir="tensor-res
 	return Xe, W, H
 end
 
-function getoldtensorresults(X::Array, nsrange; retries=1, resultdir="tensor-results", keyword="", quiet=true, isn=isnan.(X))
+function getoldtensorresults(X::Array, nsrange; retries=1, resultdir="tensor-results", keyword="", prefix="", quiet=true, isn=isnan.(X))
 	Xe = nothing
 	W = nothing
 	H = nothing
 	for ns = nsrange
-		if isdefined(rMF, :wellssetid)
-			filename = "$resultdir/$(rMF.case)-w$(rMF.wellssetid)-s$(rMF.speciessetid)-y$(rMF.tensoryearstep)-$keyword$ns-$retries.jld"
+		if prefix == ""
+			if isdefined(rMF, :wellssetid)
+				filename = "$resultdir/$(rMF.case)-w$(rMF.wellssetid)-s$(rMF.speciessetid)-y$(rMF.tensoryearstep)-$keyword$ns-$retries.jld"
+			else
+				filename = "$resultdir/$(rMF.case)-$keyword$ns-$retries.jld"
+			end
 		else
-			filename = "$resultdir/$(rMF.case)-$keyword$ns-$retries.jld"
+			filename = "$resultdir/$(prefix)-$keyword$ns-$retries.jld"
 		end
 		if isfile(filename)
 			of, W, H = JLD.load(filename, "OF", "W", "H")
